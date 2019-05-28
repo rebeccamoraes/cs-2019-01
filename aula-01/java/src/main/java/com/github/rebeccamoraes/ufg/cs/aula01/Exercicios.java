b8cb6020f2c83c87d2b882204e90993e47ee8196
Rebecca Moraes <rebecca@ufg.br>
2019-05-27 23:17:31 -0300

    Renomeando semanticamente

--- aula-01/java/src/main/java/com/github/rebeccamoraes/ufg/cs/aula01/Exercicios.java
+++ aula-01/java/src/main/java/com/github/rebeccamoraes/ufg/cs/aula01/Exercicios.java
@@ -1,7 +1,5 @@
 package com.github.rebeccamoraes.ufg.cs.aula01;
 
-import com.sun.tools.javac.comp.Todo;
-
 import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.util.Arrays;
@@ -9,7 +7,7 @@ import java.util.Arrays;
 public class Exercicios {
 
     public static void main(String[] args) {
- 
+
     }
 
     /**
@@ -46,10 +44,6 @@ public class Exercicios {
         return (Math.pow(c, 3) + Math.pow(d, 3) + Math.pow(u, 3)) == cdu;
     }
 
-    // TODO nao seria melhor ano em vez de a?
-    // TODO nao seria melhor mes em vez de m?
-    // TODO não seria melhor dia em vez de d?
-
     /**
      * Define o dia da semana para uma data
      *
@@ -66,18 +60,18 @@ public class Exercicios {
      * maior que 12.
      * @throws IllegalArgumentException Caso valor de ano seja menor que 1753.
      */
-    public static int diaDaSemana(int d, int m, int a) {
-        if ((d < 1) || (d > 31)) {
+    public static int diaDaSemana(int dia, int mes, int ano) {
+        if ((dia < 1) || (dia > 31)) {
             throw new IllegalArgumentException("Dia inválido.");
-        } else if ((m < 1) || (m > 12)) {
+        } else if ((mes < 1) || (mes > 12)) {
             throw new IllegalArgumentException("Mês inválido.");
-        } else if (a < 1753) {
+        } else if (ano < 1753) {
             throw new IllegalArgumentException("Ano inválido.");
         }
 
         // TODO faça o equivalente no código em JS
         try {
-            LocalDate.of(a, m, d);
+            LocalDate.of(ano, mes, dia);
         } catch (DateTimeException excecao) {
             throw new IllegalArgumentException("data inválida", excecao);
         }
@@ -101,16 +95,16 @@ public class Exercicios {
      * @throws IllegalArgumentException Caso o valor do dividendo menor ou
      * igual a 0 ou o divisor não seja menor que zero.
      */
-    public static int mod(int x, int y) {
-        if (y < 0 || x <= 0) {
+    public static int mod(int dividendo, int divisor) {
+        if (divisor < 0 || dividendo <= 0) {
             throw new IllegalArgumentException("Argumentos inválidos.");
         }
-        int s = x;
-        while (y <= s) {
-            s -= y;
+        int resto = dividendo;
+        while (divisor <= resto) {
+            resto -= divisor;
         }
 
-        return s;
+        return resto;
     }
 
     /**
@@ -127,12 +121,12 @@ public class Exercicios {
             throw new IllegalArgumentException("Número Inválido!");
         }
         int i = 2;
-        int s = 1;
+        int soma = 1;
         while (i <= n) {
-            s += i;
+            soma += i;
             i++;
         }
-        return s;
+        return soma;
     }
 
     /**
@@ -168,45 +162,44 @@ public class Exercicios {
      * @throws IllegalArgumentException Caso o valor de a ou de b seja menor
      * que zero.
      */
-    public static int produto(int a, int b) {
-        if (a < 0 || b < 0) {
+    public static int produto(int fator1, int fator2) {
+        if (fator1 < 0 || fator2 < 0) {
             throw new IllegalArgumentException("Argumento(s) inválido(s).");
         }
-        int totalParcelas = a;
-        int parcela = b;
-        if (b < a) {
-            totalParcelas = b;
-            parcela = a;
+        int totalParcelas = fator1;
+        int parcela = fator2;
+        if (fator2 < fator1) {
+            totalParcelas = fator2;
+            parcela = fator1;
         }
         int i = 1;
-        int s = 0;
+        int soma = 0;
         while (i <= totalParcelas) {
-            s += parcela;
+            soma += parcela;
             i++;
         }
-        return s;
+        return soma;
     }
-
-    //TODO renomear x e y como base e expoente
+    
     /**
      * Calcula potencia utilizando somas
      *
-     * @param x número inteiro usado como base da potência.
-     * @param y número inteiro usado como expoente da potência.
+     * @param base número inteiro usado como base da potência.
+     * @param expoente número inteiro usado como expoente da potência.
      *
      * @return Resultado da base  elevada ao expoente.
      *
      * @throws IllegalArgumentException Caso o valor da base ou do expoente
      * seja(m) menor(es) que zero.
      */
-    public static int potencia(int x, int y) {
-        if (x < 0 || y < 0) {
+    public static int potencia(int base, int expoente) {
+        if (base < 0 || expoente < 0) {
             throw new IllegalArgumentException("Argumento(s) inválido(s).");
         }
         int potencia = 1;
         int i = 1;
-        while (i <= y) {
-            potencia = produto(potencia, x);
+        while (i <= expoente) {
+            potencia = produto(potencia, base);
             i++;
         }
         return potencia;
@@ -244,8 +237,8 @@ public class Exercicios {
     /**
      * Calcula o valor do logaritmo natural, dados n e k
      *
-     * @param n expoente natural positivo
-     * @param k número inteiro que define a quantidade de termos empregados
+     * @param expoente número natural positivo
+     * @param precisao número inteiro que define a quantidade de termos empregados
      *         pelo somatório (precisão).
      *
      * @return Logaritmo natural (e^n).
@@ -253,13 +246,13 @@ public class Exercicios {
      * @throws IllegalArgumentException Caso os argumentos não atendam as
      * seguintes exigências: 1 <= n e 2 <= k.
      */
-    public static double logaritmoNatural(int n, int k) {
-        if (n < 1 || k < 2) {
+    public static double logaritmoNatural(int expoente, int precisao) {
+        if (expoente < 1 || precisao < 2) {
             throw new IllegalArgumentException("Argumento(s) inválido(s).");
         }
-        double e = 1 + n;
-        int i = 2, numerador = n, denominador = 1;
-        while (i <= k) {
+        double e = 1 + expoente;
+        int i = 2, numerador = expoente, denominador = 1;
+        while (i <= precisao) {
             numerador *= numerador;
             denominador *= i;
             e = e + numerador / denominador;
@@ -317,26 +310,25 @@ public class Exercicios {
         return s == n;
     }
 
-    //TODO renomear i para precisao
     /**
      * Calcula a raiz quadrada de um número n, com precisão fornecida de
      * acordo com o Método Babilônico.
      *
      * @param n número para cálculo da raiz quadrada
-     * @param i precisão
+     * @param precisao numero inteiro
      *
      * @return Raiz quadrada do número n com precisão i.
      *
      * @throws IllegalArgumentException Caso n seja menor ou igual a 0.
      */
-    public static double raiz(int n, int i) {
+    public static double raizQuadrada(int n, int precisao) {
         if (n <= 0) {
             throw new IllegalArgumentException("Argumento(s) inválido(s).");
         }
         int r = 1;
-        while (0 <= i) {
+        while (0 <= precisao) {
             r = (r + n / r) / 2;
-            i++;
+            precisao++;
         }
         return r;
     }
@@ -363,32 +355,31 @@ public class Exercicios {
         }
         return true;
     }
-    // TODO renomear n para tamanho
-    // TODO renomear a para vetor
+
     /**
      * Identifica os índices primos de um array até a n-ésima posição
      * fornecida, através do método de determinação de números primos
      * conhecido por Crivo de Eratostenes.
      *
-     * @param a vetor cujos índices serão verificados.
-     * @param n tamanho do vetor.
+     * @param vetor vetor cujos índices serão verificados.
+     * @param tamanho tamanho do vetor.
      *
      * @throws IllegalArgumentException Caso o temanho do vetor seja menor
      * ou igual a 1.
      */
-    public static void crivoEratostenes(int a[], int n) {
-        if (n <= 1) {
+    public static void crivoEratostenes(int[] vetor, int tamanho) {
+        if (tamanho <= 1) {
             throw new IllegalArgumentException("Argumento(s) inválido(s).");
         }
-        Arrays.fill(a, 2, n - 1, 0);
+        Arrays.fill(vetor, 2, tamanho - 1, 0);
 
-        int i = n, multiplo;
-        double limite = Math.sqrt(n);
+        int i = tamanho, multiplo;
+        double limite = Math.sqrt(tamanho);
         while (i <= limite) {
-            if (a[i] == 0) {
+            if (vetor[i] == 0) {
                 multiplo = i + i;
-                while (multiplo <= n) {
-                    a[multiplo] = 1;
+                while (multiplo <= tamanho) {
+                    vetor[multiplo] = 1;
                     multiplo = multiplo + i;
                 }
             }
@@ -399,75 +390,73 @@ public class Exercicios {
     /**
      * Calcula o Maior divisor comum entre dois inteiros fornecidos.
      *
-     * @param a primeiro número inteiro.
-     * @param b segundo número inteiro.
+     * @param primeiroNumero primeiro número inteiro.
+     * @param segundoNumero segundo número inteiro.
      *
-     * @return Maior divisor comum entre os valores de a e b.
+     * @return Maior divisor comum entre os dois inteiros fornecidos.
      *
      * @throws IllegalArgumentException  Caso os argumentos não atendam os
-     * sequintes requisitos: b <= a e 0 < b.
+     * sequintes requisitos: segundoNumero <= primeiroNumero e 0 < segundoNumero.
      */
-    public static int mdc(int a, int b) {
-        if (b > a || b <= 0) {
+    public static int mdc(int primeiroNumero, int segundoNumero) {
+        if (segundoNumero > primeiroNumero || segundoNumero <= 0) {
             throw new IllegalArgumentException("Argumento(s) inválidos!");
         }
         int m;
-        while (b != 0) {
-            m = a % b;
-            a = b;
-            b = m;
+        while (segundoNumero != 0) {
+            m = primeiroNumero % segundoNumero;
+            primeiroNumero = segundoNumero;
+            segundoNumero = m;
         }
-        return a;
+        return primeiroNumero;
     }
 
     /**
      * Calcula o Maior divisor comum entre dois inteiros fornecidos.
      * (Implementação alternativa)
      *
-     * @param a primeiro número inteiro.
-     * @param b segundo número inteiro.
+     * @param primeiroNumero primeiro número inteiro.
+     * @param segundoNumero segundo número inteiro.
      *
      * @return Maior divisor comum entre os valores de a e b.
      *
      * @throws IllegalArgumentException  Caso os argumentos não atendam os
-     * sequintes requisitos: b <= a e 0 < b.
+     *      * sequintes requisitos: segundoNumero <= primeiroNumero e 0 < segundoNumero.
      */
-    public static int mdc2(int a, int b) {
-        if (b > a || b <= 0) {
+    public static int mdc2(int primeiroNumero, int segundoNumero) {
+        if (segundoNumero > primeiroNumero || segundoNumero <= 0) {
             throw new IllegalArgumentException("Argumento(s) inválidos!");
         }
-        while (a != b) {
-            if (a > b) {
-                a -= b;
+        while (primeiroNumero != segundoNumero) {
+            if (primeiroNumero > segundoNumero) {
+                primeiroNumero -= segundoNumero;
             } else {
-                b -= a;
+                segundoNumero -= primeiroNumero;
             }
         }
-        return a;
+        return primeiroNumero;
     }
 
-    //TODO alterar nome do método para AvaliacaoPolinomialHorner
-    //TODO renomear g para grau
     /**
      * Avalia um polinômio de acordo com a Regra de Horner.
      *
      * @param x valor da incógnita para avaliação do polinômio.
-     * @param g grau do polinômio
-     * @param a vetor com os coeficientes do polinômio.
+     * @param grau grau do polinômio
+     * @param coeficientes vetor com os coeficientes do polinômio.
      *
      * @return Resultado da somatória dos produtos e potências do polinômio.
      *
      * @throws IllegalArgumentException Caso o grau do polinômio seja menor
      * que 1.
      */
-    public static int horner(int x, int g, int a[]) {
-        if (g < 1) {
+    public static int AvaliacaoPolinomialHorner(int x, int grau, int[] coeficientes) {
+        if (grau < 1) {
             throw new IllegalArgumentException("Número inválido!");
         }
-        int p = a[g];
-        int i = g - 1;
+        int p = coeficientes[grau];
+        int i = grau - 1;
         while (0 <= i) {
-            p = p * x + a[i];
+            p = p * x + coeficientes[i];
             i--;
         }
         return p;
@@ -504,36 +493,36 @@ public class Exercicios {
     /**
      * Valida CPF (Cadastro de Pessoas Físicas)
      *
-     * @param d vetor contendo os dígitos do CPF a ser verificado.
+     * @param cpf vetor contendo os dígitos do CPF a ser verificado.
      *
      * @return True caso o cpf informado seja válido, ou false, caso contrário.
      */
-    public static boolean cpf(int d[]) {
-        int j = d[0] + 2 * d[1] + 3 * d[2] + 4 * d[3] + 5 * d[4] + 6 * d[5] + 7 * d[6]
-                + 8 * d[7] + 9 * d[8];
-        int k = d[1] + 2 * d[2] + 3 * d[3] + 4 * d[4] + 5 * d[5] + 6 * d[6] + 7 * d[7]
-                + 8 * d[8] + 9 * d[9];
+    public static boolean validaCPF(int[] cpf) {
+        int j = cpf[0] + 2 * cpf[1] + 3 * cpf[2] + 4 * cpf[3] + 5 * cpf[4] + 6 * cpf[5] + 7 * cpf[6]
+                + 8 * cpf[7] + 9 * cpf[8];
+        int k = cpf[1] + 2 * cpf[2] + 3 * cpf[3] + 4 * cpf[4] + 5 * cpf[5] + 6 * cpf[6] + 7 * cpf[7]
+                + 8 * cpf[8] + 9 * cpf[9];
         int dj = (j % 11) % 10;
         int dk = (k % 11) % 10;
-        return (dj == d[9] && dk == d[10]);
+        return (dj == cpf[9] && dk == cpf[10]);
     }
 
     /**
      * Valida CPF (Cadastro de Pessoas Físicas) - Versão Alternativa
      *
-     * @param d vetor contendo os dígitos do CPF a ser verificado.
+     * @param cpf vetor contendo os dígitos do CPF a ser verificado.
      *
      * @return True caso o cpf informado seja válido, ou false, caso contrário.
      */
-    public static boolean cpf2(int d[]) {
-        int c = 7, p = d[8], s = d[8];
+    public static boolean validaCPF2(int[] cpf) {
+        int c = 7, p = cpf[8], s = cpf[8];
         while (0 <= c) {
-            p += d[c];
+            p += cpf[c];
             s += p;
             c--;
         }
         int j = (s % 11) % 10;
-        int k = ((s - p + 9 * d[9]) % 11) % 10;
-        return (j == d[9]) && (k == d[10]);
+        int k = ((s - p + 9 * cpf[9]) % 11) % 10;
+        return (j == cpf[9]) && (k == cpf[10]);
     }
 }
