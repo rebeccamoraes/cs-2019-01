085052dd54763308345bfa93c675a0ce3e5c9a2c
Rebecca Moraes <rebecca@ufg.br>
2019-06-13 23:40:21 -0300

    Adicionando testes da função logaritmoNatural com 100% de cobertura.

---
+++ aula-01/java/src/test/java/com/github/rebeccamoraes/ufg/cs/aula01/LogaritmoNaturalTest.java
@@ -0,0 +1,26 @@
+package com.github.rebeccamoraes.ufg.cs.aula01;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class LogaritmoNaturalTest {
+
+    @Test
+    void expoenteMenorQue1GeraExcecao(){
+        assertThrows(IllegalArgumentException.class,
+                () -> Exercicios.logaritmoNatural(0, 3));
+    }
+
+    @Test
+    void precisaoMenorQue2GeraExcecao(){
+        assertThrows(IllegalArgumentException.class,
+                () -> Exercicios.logaritmoNatural(1, 1));
+    }
+
+    @Test
+    void logaritmoNaturalDe2(){
+        assertEquals(5, Exercicios.logaritmoNatural(2, 2));
+    }
+}
