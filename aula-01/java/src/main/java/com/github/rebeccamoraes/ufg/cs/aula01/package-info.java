555972070225e4809d95cb8e6868024f0a93f96d
Rebecca Moraes <rebecca@ufg.br>
2019-06-16 13:20:19 -0300

    Correções apontadas pelo checkstyle.

---
+++ aula-01/java/src/main/java/com/github/rebeccamoraes/ufg/cs/aula01/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Pacote do repositório de "exercicio".
+ * Contém uma classe {@link com.github.rebeccamoraes.ufg.cs.aula01.Exercicios}
+ *
+ * @see com.github.rebeccamoraes.ufg.cs.aula01.Exercicios
+ *
+ */
+package com.github.rebeccamoraes.ufg.cs.aula01;
