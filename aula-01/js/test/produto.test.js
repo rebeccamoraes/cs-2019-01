e5a01f2c4f343f7dff2ba0cf1dd92f14cbab7ce0
Rebecca Moraes <rebecca@ufg.br>
2019-06-18 07:33:55 -0300

    Adicionados testes para a função produto na versão javascript.

---
+++ aula-01/js/test/produto.test.js
@@ -0,0 +1,17 @@
+const funcao = require("../exercicios-iniciais").produto;
+
+test("Produto de 5 e 7 = 35", () => {
+    expect( funcao(5, 7) ).toBe(35);
+});
+
+test("Produto de 7 e 5 = 35", () => {
+    expect( funcao(7, 5) ).toBe(35);
+});
+
+test("Fator 1 menor qe 0 gera exceção", () =>{
+    expect( () => { funcao(-1, 5); } );
+});
+
+test("Fator 2 menor qe 0 gera exceção", () =>{
+    expect( () => { funcao(5, -1); } );
+});
