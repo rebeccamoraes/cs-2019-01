5a86582285417701096a4bab0b787e73cbd0781f
Rebecca Moraes <rebecca@ufg.br>
2019-06-25 20:55:32 -0300

    Testes atendendo ao requisito R7: O programa deve retornar o valor -1 se não receber 4 argumentos.

--- aula-10/src/test/java/com/github/rebeccamoraes/ufg/cs/aula10/ObtemDiaDaSemanaTest.java
+++ aula-10/src/test/java/com/github/rebeccamoraes/ufg/cs/aula10/ObtemDiaDaSemanaTest.java
@@ -7,9 +7,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ObtemDiaDaSemanaTest{
 
-    final int[] argsTeste = {1, 1, 1};
+    final String[] tresArgs = {"20160928", "2016", "20160928"};
+    final String[] doisArgs = {"20160928", "2016"};
+    final String[] umArg = {"20160928"};
+
+
     @Test
     void quantidadeDeArgumentosInsuficienteRetornaMenos1(){
-        assertEquals(-1, DiaDaSemana.obtemDiaDaSemana(argsTeste));
+        assertEquals(-1, DiaDaSemana.obtemDiaDaSemana(null));
+        assertEquals(-1, DiaDaSemana.obtemDiaDaSemana(umArg));
+        assertEquals(-1, DiaDaSemana.obtemDiaDaSemana(doisArgs));
+        assertEquals(-1, DiaDaSemana.obtemDiaDaSemana(tresArgs));
     }
 }
