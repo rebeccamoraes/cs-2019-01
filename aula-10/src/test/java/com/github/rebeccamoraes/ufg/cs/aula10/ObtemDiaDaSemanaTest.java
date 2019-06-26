a8883b2f893d89ba9f4f76bc8ab219e028914e00
Rebecca Moraes <rebecca@ufg.br>
2019-06-25 21:12:58 -0300

    Adicionado testes atendendo ao requisito R8: O programa deve retornar o valor -1 se pelo menos um dos 4 argumentos recebidos não for um valor inteiro.

--- aula-10/src/test/java/com/github/rebeccamoraes/ufg/cs/aula10/ObtemDiaDaSemanaTest.java
+++ aula-10/src/test/java/com/github/rebeccamoraes/ufg/cs/aula10/ObtemDiaDaSemanaTest.java
@@ -10,13 +10,27 @@ public class ObtemDiaDaSemanaTest{
     final String[] tresArgs = {"20160928", "2016", "20160928"};
     final String[] doisArgs = {"20160928", "2016"};
     final String[] umArg = {"20160928"};
+    final String[] primeiroArgNaoInteiro = {"a", "2019", "20190101", "2016"};
+    final String[] segundoArgNaoInteiro = {"20190101", "a", "20190101", "2016"};
+    final String[] terceiroArgNaoInteiro = {"20190101", "2019", "a", "2016"};
+    final String[] quartoArgNaoInteiro = {"20190101", "2019", "20190101", "a"};
+    final String[] nenhumArgInteiro = {"1.1", "a", "b", "c"};
 
 
     @Test
     void quantidadeDeArgumentosInsuficienteRetornaMenos1(){
-        assertEquals(-1, DiaDaSemana.obtemDiaDaSemana(null));
+        //assertEquals(-1, DiaDaSemana.obtemDiaDaSemana(null));
         assertEquals(-1, DiaDaSemana.obtemDiaDaSemana(umArg));
         assertEquals(-1, DiaDaSemana.obtemDiaDaSemana(doisArgs));
         assertEquals(-1, DiaDaSemana.obtemDiaDaSemana(tresArgs));
     }
+
+    @Test
+    void umOuMaisArgumentosNaoInteiros(){
+        assertEquals(-1, DiaDaSemana.obtemDiaDaSemana(primeiroArgNaoInteiro));
+        assertEquals(-1, DiaDaSemana.obtemDiaDaSemana(segundoArgNaoInteiro));
+        assertEquals(-1, DiaDaSemana.obtemDiaDaSemana(terceiroArgNaoInteiro));
+        assertEquals(-1, DiaDaSemana.obtemDiaDaSemana(quartoArgNaoInteiro));
+        assertEquals(-1, DiaDaSemana.obtemDiaDaSemana(nenhumArgInteiro));
+    }git 
 }
