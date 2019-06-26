582db4874a713cdadd347d33a32b41f789721946
Rebecca Moraes <rebecca@ufg.br>
2019-06-25 21:20:23 -0300

    Adicionado testes atendendo ao requisito R9: O programa deve retornar -1 se pelo menos um dos 4 argumentos recebidos pelo programa for um valor negativo.

--- aula-10/src/test/java/com/github/rebeccamoraes/ufg/cs/aula10/ObtemDiaDaSemanaTest.java
+++ aula-10/src/test/java/com/github/rebeccamoraes/ufg/cs/aula10/ObtemDiaDaSemanaTest.java
@@ -10,13 +10,20 @@ public class ObtemDiaDaSemanaTest{
     final String[] tresArgs = {"20160928", "2016", "20160928"};
     final String[] doisArgs = {"20160928", "2016"};
     final String[] umArg = {"20160928"};
+
     final String[] primeiroArgNaoInteiro = {"a", "2019", "20190101", "2016"};
     final String[] segundoArgNaoInteiro = {"20190101", "a", "20190101", "2016"};
     final String[] terceiroArgNaoInteiro = {"20190101", "2019", "a", "2016"};
     final String[] quartoArgNaoInteiro = {"20190101", "2019", "20190101", "a"};
     final String[] nenhumArgInteiro = {"1.1", "a", "b", "c"};
 
+    final String[] primeiroArgNegativo = {"-1", "2019", "20190101", "2016"};
+    final String[] segundoArgNegativo = {"20190101", "-1", "20190101", "2016"};
+    final String[] terceiroArgNegativo = {"20190101", "2019", "-1", "2016"};
+    final String[] quartoArgNegativo = {"20190101", "2019", "20190101", "-1"};
+    final String[] todosArgsNegativos = {"-1", "-1", "-1", "-1"};
 
+    //R7
     @Test
     void quantidadeDeArgumentosInsuficienteRetornaMenos1(){
         //assertEquals(-1, DiaDaSemana.obtemDiaDaSemana(null));
@@ -25,12 +32,23 @@ public class ObtemDiaDaSemanaTest{
         assertEquals(-1, DiaDaSemana.obtemDiaDaSemana(tresArgs));
     }
 
+    //R8
     @Test
-    void umOuMaisArgumentosNaoInteiros(){
+    void umOuMaisArgumentosNaoInteirosRetornaMenos1(){
         assertEquals(-1, DiaDaSemana.obtemDiaDaSemana(primeiroArgNaoInteiro));
         assertEquals(-1, DiaDaSemana.obtemDiaDaSemana(segundoArgNaoInteiro));
         assertEquals(-1, DiaDaSemana.obtemDiaDaSemana(terceiroArgNaoInteiro));
         assertEquals(-1, DiaDaSemana.obtemDiaDaSemana(quartoArgNaoInteiro));
         assertEquals(-1, DiaDaSemana.obtemDiaDaSemana(nenhumArgInteiro));
-    }git 
+    }
+
+    //R9
+    @Test
+    void umOuMaisArgumentosNegativosRetornaMenos1(){
+        assertEquals(-1, DiaDaSemana.obtemDiaDaSemana(primeiroArgNegativo));
+        assertEquals(-1, DiaDaSemana.obtemDiaDaSemana(segundoArgNegativo));
+        assertEquals(-1, DiaDaSemana.obtemDiaDaSemana(terceiroArgNegativo));
+        assertEquals(-1, DiaDaSemana.obtemDiaDaSemana(quartoArgNegativo));
+        assertEquals(-1, DiaDaSemana.obtemDiaDaSemana(todosArgsNegativos));
+    }
 }
