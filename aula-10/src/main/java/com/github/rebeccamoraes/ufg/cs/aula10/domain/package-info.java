32c302b31c9520fe90bd3bbead4b2d2993b5c985
Rebecca Moraes <rebecca@ufg.br>
2019-06-27 20:50:44 -0300

    Corrigindo violações apontadas pela análise estática checkstyle.

---
+++ aula-10/src/main/java/com/github/rebeccamoraes/ufg/cs/aula10/domain/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Pacote do programa Dia da Semana. Contém duas classes:
+ * (a) DiaDaSemana
+ * (b) ConversorDeVetores
+ *
+ * @see com.github.rebeccamoraes.ufg.cs.aula10.domain.DiaDaSemana;
+ * @see com.github.rebeccamoraes.ufg.cs.aula10.domain.ConversorDeVetores;
+ */
+package com.github.rebeccamoraes.ufg.cs.aula10.domain;
