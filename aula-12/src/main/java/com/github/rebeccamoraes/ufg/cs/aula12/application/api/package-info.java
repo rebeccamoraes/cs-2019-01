35ec863e3c70a8a43c4b3d0d0bf24ff4ae096203
Rebecca Moraes <rebecca@ufg.br>
2019-07-11 20:51:56 -0300

    Zerando erros checkstyle no diretório api.

---
+++ aula-12/src/main/java/com/github/rebeccamoraes/ufg/cs/aula12/application/api/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Api para obter o valor por extenso de
+ * um número inteiro de até 4 dígitos.
+ * Contem 2 classes:
+ * (1) Application
+ * (2) NumeroController
+ */
+package com.github.rebeccamoraes.ufg.cs.aula12.application.api;
