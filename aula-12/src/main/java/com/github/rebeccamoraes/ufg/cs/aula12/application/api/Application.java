fde8cad1912f12756e96c5ce61031e77349dc592
Rebecca Moraes <rebecca@ufg.br>
2019-07-11 20:30:48 -0300

    Corrigindo erro na aplicação.

--- aula-12/src/main/java/com/github/rebeccamoraes/ufg/cs/aula12/application/api/Application.java
+++ aula-12/src/main/java/com/github/rebeccamoraes/ufg/cs/aula12/application/api/Application.java
@@ -4,7 +4,7 @@
  * Creative Commons Attribution 4.0 International License.
  */
 
-package com.github.rebeccamoraes.ufg.cs.aula11.application.api;
+package com.github.rebeccamoraes.ufg.cs.aula12.application.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
